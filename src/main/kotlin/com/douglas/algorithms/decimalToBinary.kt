d0f62620c92a5fc384507122f1dd661464fcf485
dougluz <dougs.2210@gmail.com>
2020-09-15 15:02:52 -0300

    feat: added decimalToBinary algorithm

--- src/main/kotlin/com/douglas/algorithms/decimalToBinary.kt
+++ src/main/kotlin/com/douglas/algorithms/decimalToBinary.kt
@@ -1,2 +1,21 @@
 package com.douglas.algorithms
 
+import com.douglas.datastructures.Stack
+
+fun decimalToBinary(decNumber: Int): String {
+    val stack = Stack(4)
+    var number = decNumber
+    var rem: Int
+    var binaryString = ""
+
+    if (number == 0) stack.push(number)
+    while (number > 0) {
+        rem = number % 2
+        stack.push(rem)
+        number /= 2
+    }
+    while (!stack.isEmpty()) {
+        binaryString += stack.pop().toString()
+    }
+    return binaryString
+}
--- src/test/kotlin/com/douglas/algorithms/DecimalToBinaryTest.kt
+++ src/test/kotlin/com/douglas/algorithms/DecimalToBinaryTest.kt
@@ -1,2 +1,21 @@
 package com.douglas.algorithms
 
+import org.junit.Assert
+import org.junit.Test
+
+class DecimalToBinaryTest {
+    @Test
+    fun `should return the correct decimal value`() {
+        Assert.assertEquals("0", decimalToBinary(0))
+        Assert.assertEquals("1", decimalToBinary(1))
+        Assert.assertEquals("10", decimalToBinary(2))
+        Assert.assertEquals("11", decimalToBinary(3))
+        Assert.assertEquals("100", decimalToBinary(4))
+        Assert.assertEquals("101", decimalToBinary(5))
+        Assert.assertEquals("110", decimalToBinary(6))
+        Assert.assertEquals("111", decimalToBinary(7))
+        Assert.assertEquals("1000", decimalToBinary(8))
+        Assert.assertEquals("1001", decimalToBinary(9))
+        Assert.assertEquals("1010", decimalToBinary(10))
+    }
+}
